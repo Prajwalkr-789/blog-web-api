bf7ec5924ef18e217a92df3c6234ee7d247b6dd4
Prajwal-kr789 <prajwal07kr@gmail.com>
2024-06-03 17:26:39 +0530

    Updated vercel

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 8000;
 const routes = require('./routes/route');
-const cors = require('cors')
-const blogroute = require('./routes/blogroute')
-const  mongoose = require('mongoose');
+const cors = require('cors');
+const blogroute = require('./routes/blogroute');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
-app.use(express.json({limit : '2mb'}))
-app.use(cookieParser({limit : '2mb'}))
-app.use(cors())
-app.use('/user',routes)
-app.use('/blog',blogroute)
-app.get('/',(req,res)=>{
-    res.json('Jai shree ram')
-})
-const dburl = 'mongodb+srv://prajwalkrp07:SVOzVfzrl7mJiUyA@cluster0.jpkht3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+app.use(express.json({ limit: '2mb' }));
+app.use(cookieParser({ limit: '2mb' }));
+app.use(cors());
+app.use('/user', routes);
+app.use('/blog', blogroute);
+app.get('/', (req, res) => {
+    res.json('Jai shree ram');
+});
+
+const dburl = 'mongodb+srv://prajwalkrp07:SVOzVfzrl7mJiUyA@cluster0.jpkht3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
 mongoose.connect(dburl)
-        .then(()=>{
-            app.listen(PORT,()=>{console.log("Server started and connected to database...")})
-        })
-        .catch(()=>{
-            console.log("Something happened...")
-        })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log("Server started and connected to database...");
+        });
+    })
+    .catch((err) => {
+        console.error("Something happened...", err);
+    });
+
+// Export the app for Vercel
+module.exports = app;
